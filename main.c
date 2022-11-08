ac3767d0309901422f418d23cf807628fc312f36
Joel king yan Hii <jhii@u82z05s02.42kl.edu.my>
2022-11-08 13:25:40 +0800

    modified main.c

--- main.c
+++ main.c
@@ -6,17 +6,14 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 17:49:43 by jhii              #+#    #+#             */
-/*   Updated: 2022/11/08 13:20:54 by jhii             ###   ########.fr       */
+/*   Updated: 2022/11/08 13:25:08 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/sandbox.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	(void)argv;
-	if (argc != 2)
-		return(printf("Error: Invalid number of arguments..\n"));
 	sandbox();
 	return (0);
 }
